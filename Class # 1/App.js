97888ece31feccb01c76e5d0263ff108a6e52108
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-03-17 13:18:22 +0500

    Add data

--- Class # 1/App.js
+++ Class # 1/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
 
 export default function App() {
+
+  const [inputData, setInputData] = useState('')
+  const [todolist, setTodoList] = useState([])
+
+  const AddTodo = ()=>{
+    setTodoList(prevValue => [...prevValue, inputData])
+    console.log(inputData);
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <View>
+      <TextInput style={styles.input} value={inputData} onChange={(e)=> setInputData(e.target.value)}/>
+      <Button title='ADD' onPress={AddTodo}/>
+      </View>
+      <View>
+        {todolist.map((item, index)=>(
+          <Text key={item + index}>{item}</Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -17,4 +33,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input :{
+    borderColor: '#000',
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    marginBottom: 10
+   }
 });
